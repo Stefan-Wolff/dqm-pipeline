ddb348576534ba0ef20a73b05e74c67ef702b399
Stefan Wolff <stefanwolff@gmx.de>
2023-07-25 09:25:57 +0200

    add monitoring

---
+++ monitor.py
@@ -0,0 +1,77 @@
+import os
+import json
+
+
+class Monitor():
+
+	QUALITY_FILE = "repo/quality_0.json"
+	MONITOR_FILE = "repo/monitor.json"
+
+	def run(self):
+		# init repo files
+		with open(Monitor.QUALITY_FILE, 'r', encoding='utf-8') as qualityIn:
+			quality = json.load(qualityIn)
+			
+		monitor = []
+		
+		if os.path.exists(Monitor.MONITOR_FILE):
+			with open(Monitor.MONITOR_FILE, 'r', encoding='utf-8') as monitorIn:
+				monitor = json.load(monitorIn)
+			
+		# order analyze entries by data name
+		ordered = {}
+		for entry in quality:
+			data_name = entry["chain"]
+			ordered.setdefault(data_name, {})
+			ordered[data_name].setdefault(entry["metrics"], [])
+			ordered[data_name][entry["metrics"]].append(entry)
+			
+
+		# build diffs
+		for dim in ["Completeness", "Correctness", "Consistency"]:
+			monitor.append(self._createDiff(ordered["initial"][dim][-1], ordered["initial"][dim][-2]))
+			
+		
+		for dim in ["Completeness", "Correctness", "Consistency"]:
+			increase_before = self._createDiff(ordered["initial"][dim][-2], ordered["complete"][dim][-2])
+			increase_before["time"] = increase_before["time_2"]
+			increase_before["metrics"] = increase_before["metrics_2"]
+			increase_before["chain"] = "diff:" + increase_before["chain_1"] + "-" + increase_before["chain_2"]
+			
+			increase_last = self._createDiff(ordered["initial"][dim][-1], ordered["complete"][dim][-1])
+			increase_last["time"] = increase_last["time_2"]
+			increase_last["metrics"] = increase_last["metrics_2"]
+			increase_last["chain"] = "diff:" + increase_last["chain_1"] + "-" + increase_last["chain_2"]
+			
+			monitor.append(self._createDiff(increase_before, increase_last))
+		
+			
+		# save
+		with open(Monitor.MONITOR_FILE, 'w') as outFile:
+			json_string = json.dumps(monitor, indent=4)
+			outFile.write(json_string)
+		
+	def _createDiff(self, first, second):
+		return {
+			"time_1": first["time"],
+			"time_2": second["time"],
+			"metrics": first["metrics"],
+			"chain_1": first["chain"],
+			"chain_2": second["chain"],
+			"counts": self._diff(first["counts"], second["counts"]),
+			"indicators": self._diff(first["indicators"], second["indicators"])
+		}
+	
+		
+	def _diff(self, first, second):
+		result = {}
+		
+		for key, value in first.items():
+			result[key] = round(value - second[key], 3)
+			
+		return result
+
+
+### entry
+if "__main__" == __name__:
+	Monitor().run()
--- transform.py
+++ transform.py
@@ -8,6 +8,24 @@ from lib.data_processor import DataProcessor
 
 
 class Transformer(DataProcessor):
+
+	FULL_CHAIN = [
+		"ParseBibtex",
+		"ParseValues",
+		"CorrectOrgs",
+		"CorrectMinLength",
+		"CorrectValues",
+		"CorrectContradict",
+		"JoinCrossRef",
+		"ParseValues",
+		"CorrectMinLength",
+		"CorrectValues",
+		"CorrectContradict",
+		"Merge",
+		"FilterContradict",
+		"FilterObjects"
+	]
+
 	def _run(self, dataFrames, config, spark):
 		transforms = {
 			"ParseBibtex": ParseBibtex(),
@@ -25,22 +43,44 @@ class Transformer(DataProcessor):
 			"Filter": Filter()
 		}
 		
-		transform_impl = transforms[config.transformation]
-		df_results = transform_impl.run(dataFrames, spark)
-		for entity, df in df_results.items():
+		# complete chain
+		if "complete" == config.transformation:
+			print("running complete pipeline: " + str(Transformer.FULL_CHAIN))
+			
+			entities = set()
+			for transform_task in Transformer.FULL_CHAIN:
+				cur_results = transforms[transform_task].run(dataFrames, spark)
+				dataFrames.update(cur_results)
+				entities.update(cur_results.keys())
+			
+			df_results = {e: dataFrames[e] for e in entities}						# save returned dataframes only
+			new_chain = config.transformation
+			
+			
+		# single transformation
+		else:
+			transform_impl = transforms[config.transformation]
+			df_results = transform_impl.run(dataFrames, spark)
+			
 			class_name = type(transform_impl).__name__
 			new_chain = config.chain + "." + class_name if ("initial" != config.chain) else class_name
-
+		
+		
+		# write results
+		for entity, df in df_results.items():
 			outPath = "data/parquets/" + new_chain + "/" + entity
 			df.write.parquet(outPath, mode="overwrite")
 			print(outPath + " written")
+		
+	
+	
 
 ### entry
 if "__main__" == __name__:
 
 	# init parameters
 	parser = argparse.ArgumentParser(prog='Data Transformer', description='Runs given transformation')
-	parser.add_argument('-t', '--transformation', help='the transformation to run', required=True)
+	parser.add_argument('-t', '--transformation', help='the transformation to run', default='complete')
 	parser.add_argument('-c', '--chain', help='source chain if analyze transformed data', default='initial')
 					
 	Transformer().run(parser.parse_args())
