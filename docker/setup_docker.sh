6497608f36c8e58e120c1a7e25ba58a0803ee0d1
Stefan Wolff <stefanwolff@gmx.de>
2023-08-27 13:08:35 +0200

    set timezone

--- docker/setup_docker.sh
+++ docker/setup_docker.sh
@@ -1,2 +1,2 @@
-docker build -t wqp docker
-docker run --name wqp -v $(pwd):/usr/local/processing/dqm-pipeline -it wqp
+docker build -t dqm docker
+docker run --name dqm -v $(pwd):/usr/local/processing/dqm-pipeline -it dqm
